be857bf201cbf77c96cd7053fd8a4174e86c54ff
José Miguel  Melo <josemiguelmelo94@gmail.com>
2019-12-10 21:18:14 +0000

    Parse CodeNarc XML result into CodeNarcOutput

--- src/main/scala/codacy/codenarc/CodeNarcOutput.scala
+++ src/main/scala/codacy/codenarc/CodeNarcOutput.scala
@@ -2,18 +2,56 @@ package codacy.codenarc
 
 import better.files.File
 
-import scala.xml.{Elem, XML}
+import scala.util.Try
+import scala.xml.{Elem, XML, Node}
 
 object CodeNarcOutput {
 
+  private val packageNode = "Package"
+  private val packagePathValue = "@path"
+  private val fileNode = "File"
+  private val fileNameValue = "@name"
+  private val violationNode = "Violation"
+  private val violationRuleNameValue = "@ruleName"
+  private val violationMessageNode = "Message"
+  private val violationLineNumber = "@lineNumber"
+
   case class CodeNarcOutput(file: String, message: String, ruleName: String, line: Int)
+
+  case class CodeNarcViolation(ruleName: String, message: String, line: Int)
 
   def loadXMLResult(resultFile: File): Elem = XML.loadString(resultFile.contentAsString)
 
   def parseResult(resultFile: File): List[CodeNarcOutput] = {
     val xmlResult = loadXMLResult(resultFile)
-    parseXmlResult(xmlResult)
+    parseXmlResult(xmlResult).toList
   }
 
-  private def parseXmlResult(resultXml: Elem): List[CodeNarcOutput] = ???
+  private def violationRuleName(violationNode: Node) = (violationNode \ violationRuleNameValue).text
+
+  private def violationMessage(violationNode: Node) = (violationNode \ violationMessageNode).text
+
+  private def violationLine(violationNode: Node) = Try((violationNode \ violationLineNumber).toString.toInt).getOrElse(0)
+
+  private def getViolationInfo(violation: Node): CodeNarcViolation = CodeNarcViolation(violationRuleName(violation), violationMessage(violation), violationLine(violation))
+
+  private def getFilenameFromFileNode(fileNode: Node, packagePath: String) = s"$packagePath/${(fileNode \ fileNameValue).text}"
+
+  private def getPackagePathFromFileNode(packageNode: Node) = (packageNode \ packagePathValue).text
+
+  private def parseXmlResult(resultXml: Elem): Seq[CodeNarcOutput] =
+    for {
+      // go over all packages
+      pkg <- resultXml \ packageNode
+      // go over files inside each package
+      file <- pkg \ fileNode
+      // get all violations for each file
+      violation <- file \ violationNode
+    } yield {
+      val packagePath = getPackagePathFromFileNode(pkg)
+      val filename = getFilenameFromFileNode(file, packagePath)
+      val violationInfo = getViolationInfo(violation)
+
+      CodeNarcOutput(filename, violationInfo.message, violationInfo.ruleName, violationInfo.line)
+    }
 }
